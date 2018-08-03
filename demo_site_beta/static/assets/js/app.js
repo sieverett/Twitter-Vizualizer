5a8a2ed4c0610afe75d2a45c3173d8257daba9d2
unknown <sieverett>
2018-08-02 20:20:27 -0700

    site beta to repo

--- demo_site_beta/static/assets/js/app.js
+++ demo_site_beta/static/assets/js/app.js
@@ -303,96 +303,53 @@ function lineGraph() {
 }
 
 function showGauge() {
-  var campaignTarget_followers = 1000000
-  var campaignTarget_friends = 10000
-  var campaignTarget_retweet_count = 1000
+  var config3 = liquidFillGaugeDefaultSettings();
+                   config3.textVertPosition = 0.5
+                   config3.waveAnimateTime = 1000;
+  config3.valueCountUp = true;
+                   config3.displayPercent = true;
+                   config3.waveCount = 1;
 
-  d3.csv("static/assets/js/gauge_data.csv", function (data) {
-    for (var i = 0; i < data.length; i++) {
-      console.log(data[i].followers);
-      console.log(data[i].friends);
-      console.log(data[i].retweet_count);
+  d3.csv("static/assets/js/gauge_data.csv", function(error, data) {
+    if (error) throw error;
+    console.log(data[0]);
 
-      var gauge1 = loadLiquidFillGauge("fillgauge1", Math.round(data[0].followers / campaignTarget_followers));
-      var gauge2 = loadLiquidFillGauge("fillgauge2", Math.round(data[0].friends / campaignTarget_friends));
-      var gauge3 = loadLiquidFillGauge("fillgauge3", Math.round(data[0].retweet_count / campaignTarget_retweet_count));
-    }
+      var foll_target = 90000000;
+      var frnd_target = 500000;
+      var retwt_target = 100000
+      var foll_i = 0;
+      var frnd_i = 0;
+      var retwt_i = 0;
+
+      var gauge4 = loadLiquidFillGauge("fillgauge4", ((data[0].followers/foll_target)* 100), config3);
+                       function NewValue() {
+                           if (Math.random() > .5) {
+                               return Math.round(Math.random() * 100);
+                           } else {
+                               return (Math.random() * 100).toFixed(1);
+                           }
+                       }
+      var gauge1 = loadLiquidFillGauge("fillgauge1", ((data[0].friends/frnd_target) * 100), config3);
+                       function NewValue() {
+                           if (Math.random() > .5) {
+                               return Math.round(Math.random() * 100);
+                           } else {
+                               return (Math.random() * 100).toFixed(1);
+                           }
+                       }
+
+      var gauge2 = loadLiquidFillGauge("fillgauge2", ((data[0].retweet_count/retwt_target) * 100), config3);
+                       function NewValue() {
+                           if (Math.random() > .5) {
+                               return Math.round(Math.random() * 100);
+                           } else {
+                               return (Math.random() * 100).toFixed(1);
+                           }
+                       }
+
   });
-
-
-  //    var gauge1 = loadLiquidFillGauge("fillgauge1", 60);
-  var config1 = liquidFillGaugeDefaultSettings();
-  config1.circleThickness = 0.4;
-  config1.circleColor = "#6DA398";
-  config1.textColor = "#0E5144";
-  config1.waveTextColor = "#6DA398";
-  config1.waveColor = "#246D5F";
-  config1.textVertPosition = 0.52;
-  config1.waveAnimateTime = 1000;
-  config1.waveHeight = 0;
-  config1.waveAnimate = false;
-  config1.waveCount = 2;
-  config1.waveOffset = 0.25;
-  config1.textSize = 1.2;
-  config1.minValue = 30;
-  config1.maxValue = 150
-  config1.displayPercent = false;
-
-  var config2 = liquidFillGaugeDefaultSettings();
-  config2.circleColor = "#D4AB6A";
-  config2.textColor = "#553300";
-  config2.waveTextColor = "#805615";
-  config2.waveColor = "#AA7D39";
-  config2.circleThickness = 0.1;
-  config2.circleFillGap = 0.2;
-  config2.textVertPosition = 0.8;
-  config2.waveAnimateTime = 2000;
-  config2.waveHeight = 0.3;
-  config2.waveCount = 1;
-
-  var config4 = liquidFillGaugeDefaultSettings();
-  config4.circleThickness = 0.15;
-  config4.circleColor = "#808015";
-  config4.textColor = "#555500";
-  config4.waveTextColor = "#FFFFAA";
-  config4.waveColor = "#AAAA39";
-  config4.textVertPosition = 0.8;
-  config4.waveAnimateTime = 1000;
-  config4.waveHeight = 0.05;
-  config4.waveAnimate = true;
-  config4.waveRise = false;
-  config4.waveHeightScaling = false;
-  config4.waveOffset = 0.25;
-  config4.textSize = 0.75;
-  config4.waveCount = 3;
-
-  var config5 = liquidFillGaugeDefaultSettings();
-  config5.circleThickness = 0.4;
-  config5.circleColor = "#6DA398";
-  config5.textColor = "#0E5144";
-  config5.waveTextColor = "#6DA398";
-  config5.waveColor = "#246D5F";
-  config5.textVertPosition = 0.52;
-  config5.waveAnimateTime = 5000;
-  config5.waveHeight = 0;
-  config5.waveAnimate = false;
-  config5.waveCount = 2;
-  config5.waveOffset = 0.25;
-  config5.textSize = 1.2;
-  config5.minValue = 30;
-  config5.maxValue = 150
-  config5.displayPercent = false;
-
-
-  function NewValue() {
-    if (Math.random() > .5) {
-      return Math.round(Math.random() * 100);
-    } else {
-      return (Math.random() * 100).toFixed(1);
-    }
-  }
 }
 
 forceGraph();
 lineGraph();
-// showGauge();
+showGauge();
